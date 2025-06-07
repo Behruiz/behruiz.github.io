c33c7ab032f9eaf577d949a7cf795c8eeeb2a819
Behruiz <75085555+Behruiz@users.noreply.github.com>
2025-06-07 00:36:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-document.getElementById("yesBtn").addEventListener("click", function() {
-    document.getElementById("responseMessage").innerHTML = "🥳 Agora posso oficialmente te chamar de MINHA princesa! 💖";
+document.getElementById("acceptBtn").addEventListener("click", function() {
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("questionScreen").style.display = "block";
 });
 
-// Ajuste para celular: o botão "Não sei..." se move quando tocado
+document.getElementById("yesBtn").addEventListener("click", function() {
+    document.getElementById("responseMessage").innerHTML = "🥳 Que alegria! Mal posso esperar por essa jornada juntos! 💖";
+});
+
+// Para celular, faz o botão "Não sei..." se mover quando tocado
 document.getElementById("noBtn").addEventListener("touchstart", function() {
     let maxX = window.innerWidth - this.clientWidth;
     let maxY = window.innerHeight - this.clientHeight;
-    
+
     let newX = Math.floor(Math.random() * maxX);
     let newY = Math.floor(Math.random() * maxY);
-    
+
     this.style.left = newX + "px";
     this.style.top = newY + "px";
 });
